373ae1f60cab37e4798a6b47d07a12b0778ede47
sanshengshui <lovewsic@gmail.com>
2021-03-23 19:27:59 +0800

    :tada: begin fingerbot-demo project

---
+++ src/main/java/com/tuya/tuyacloudfingerbotdemo/TuyaCloudFingerbotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tuya.tuyacloudfingerbotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuyaCloudFingerbotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuyaCloudFingerbotDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuya/tuyacloudfingerbotdemo/TuyaCloudFingerbotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuya.tuyacloudfingerbotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuyaCloudFingerbotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
