89ed2f08cecaedebaa41310c9fc3965d726c798f
sanshengshui <lovewsic@gmail.com>
2021-03-25 11:33:59 +0800

    :art: improve code

--- src/main/java/com/tuya/cloud/FigerbotMqExample.java
+++ src/main/java/com/tuya/cloud/FigerbotMqExample.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author jamesmsw
@@ -32,10 +33,12 @@ public class FigerbotMqExample {
     @Value("${mq.debug}")
     private Boolean debug;
 
+    private PulsarClient client;
 
     @PostConstruct
     public void init() throws Exception {
-        PulsarClient client = PulsarClient.builder().serviceUrl(serverUrl).allowTlsInsecureConnection(true)
+        log.info("Starting Server");
+        client = PulsarClient.builder().serviceUrl(serverUrl).allowTlsInsecureConnection(true)
                 .authentication(new FigerbotMqAuthentication(accessId, accessKey)).build();
         Consumer consumer = client.newConsumer().topic(String.format("%s/out/%s", accessId, env))
                 .subscriptionName(String.format("%s-sub", accessId)).subscriptionType(SubscriptionType.Failover)
@@ -55,14 +58,24 @@ public class FigerbotMqExample {
         } while (true);
     }
 
+    @PreDestroy
+    public void shutdown() {
+        log.info("stoping mq consumer");
+        try {
+            client.shutdown();
+        } catch (PulsarClientException e) {
+            log.info("mq consumer stopped!");
+        }
+    }
+
     public void onMessageArrived(Message message) throws Exception {
-        System.out.println("---------------------------------------------------");
-        System.out.println("Message received:" + new String(message.getData()) + ",seq="
+        log.info("---------------------------------------------------");
+        log.info("Message received:" + new String(message.getData()) + ",seq="
                 + message.getSequenceId() + ",time=" + message.getPublishTime() + ",consumed time="
                 + System.currentTimeMillis());
         String jsonMessage = new String(message.getData());
         FigerbotMessage vo = JSON.parseObject(jsonMessage, FigerbotMessage.class);
-        System.out.println("the real message data:" + AESBase64Utils.decrypt(vo.getData(), accessKey.substring(8, 24)));
+        log.info("the real message data:" + AESBase64Utils.decrypt(vo.getData(), accessKey.substring(8, 24)));
     }
 
 }
